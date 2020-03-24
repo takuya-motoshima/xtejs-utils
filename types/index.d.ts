9be81b61897c2b9a204a13ab6df0149b91ff7504
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2020-03-24 21:52:47 +0900

    Added graphic module type definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,10 @@
 import { version } from '~/package';
 import Misc from '~/Misc';
-import Geometry from '~/Geometry';
+import Graphics from '~/Graphics';
 import Types from '~/Types';
 import Browser from '~/Browser';
 import Datetime from '~/Datetime';
 import Random from '~/Random';
 import Collection from '~/Collection';
 import Media from '~/Media';
-export { version, Misc, Geometry, Types, Browser, Datetime, Random, Collection, Media };
+export { version, Misc, Graphics, Types, Browser, Datetime, Random, Collection, Media };
