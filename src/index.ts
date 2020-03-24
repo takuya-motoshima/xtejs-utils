a5400293616da18bb3fe2a9dfe2c0e3d1165d3b8
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2020-03-24 21:42:49 +0900

    Renamed Gemetory module to Graphics

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { version } from '~/package';
 import Misc from '~/Misc';
-import Geometry from '~/Geometry';
+import Graphics from '~/Graphics';
 import Types from '~/Types';
 import Browser from '~/Browser';
 import Datetime from '~/Datetime';
@@ -11,7 +11,7 @@ import Media from '~/Media';
 export {
   version,
   Misc,
-  Geometry,
+  Graphics,
   Types,
   Browser,
   Datetime,
